bd921e6878f34a84325490494979cb8bd17063cb
Biraj Pathak <birajpathak83@gmail.com>
2026-05-21 00:16:59 +0545

    Refactor Day 10.py with corrected formatting

--- # Day 10.py
+++ # Day 10.py
@@ -1,29 +1,29 @@
 # Day 10
-# 1.Find the intersection of two lists without set()
-# list1 = [3, 4, 5, 6, 7]
-# list2 = [1, 2, 3, 4, 5]
-# lis = []
+1.Find the intersection of two lists without set()
+list1 = [3, 4, 5, 6, 7]
+list2 = [1, 2, 3, 4, 5]
+lis = []
 
-# for i in range(len(list1)):
-#     for j in range(len(list2)):
-#         if list1[i] == list2[j] and list[i] not in lis:
-#             lis.append(list1[i])#line written for duplicate 
-#             #edge case 
-# print(lis)
+for i in range(len(list1)):
+    for j in range(len(list2)):
+        if list1[i] == list2[j] and list[i] not in lis:
+            lis.append(list1[i])#line written for duplicate 
+            #edge case 
+print(lis)
 
 # 2. dict Sort a dictionary by its values
-# dic= {"Alice":82,"Bob":90,"Ram":70,"Shyam":30}
-# result = {}
-# lis = []
-# for value in dic.values():
-#     lis.append(value)
-# lis = sorted(lis)
-# for i in range(len(lis)):
-#     for key in dic.keys():#without .keys also it acess key
-#         if dic[key] == lis[i]:
-#             result[key] = lis[i]
-# print(result)
-# print ascii uppercase with ord()
+dic= {"Alice":82,"Bob":90,"Ram":70,"Shyam":30}
+result = {}
+lis = []
+for value in dic.values():
+    lis.append(value)
+lis = sorted(lis)
+for i in range(len(lis)):
+    for key in dic.keys():#without .keys also it acess key
+        if dic[key] == lis[i]:
+            result[key] = lis[i]
+print(result)
+print ascii uppercase with ord()
 import string#ord give it's value manage by python
 for letter in string.ascii_uppercase:
     print(f"{letter} has a value of {ord(letter)}")
