001ef1a49ef2eef8225ffa1621abc56a79ecb907
Biraj Pathak <birajpathak83@gmail.com>
2026-05-24 21:16:37 +0545

    Add files via upload
    Successfully survive day 15

---
+++ # Day 15.py
@@ -0,0 +1,30 @@
+# Day 15
+# 1.Remove all duplicate characters from a string
+#manual way
+# word = input("Enter the string  ").lower()
+# result = ""
+# for i in word:
+#     if i not in result:
+#         result += i
+# print(result)
+# #pythonic way
+# result1 = "".join(dict.fromkeys(word))
+# print(result1)
+
+# 2.Find keys with the maximum value in a dict
+dic = marks = {
+    "Alice": 85,
+    "Bob": 92,
+    "Charlie": 78,
+    "David": 92,
+    "Eve": 88
+}
+lis = [] #direc without list max(dic.values())give outptu same as list without indexing as cannot be used
+for value  in dic.values():
+    lis.append(value)
+    result = max(lis)  #dic.values()
+for key,value in dic.items():# dict_values([85, 92, 78, 92, 88])
+    if value == result:
+        print(f"{key}:{value}")
+
+
